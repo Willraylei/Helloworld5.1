aa1510d5e8dfd8f09f3328c1e5902daf01deb245
LeiBin <leibin@Alex-Lei-2.local>
2017-04-22 12:10:13 +0800

    Initial commit

---
+++ app/src/main/java/com/example/leibin/helloworld51/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.leibin.helloworld51;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView web;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        web= (WebView) findViewById(R.id.web);
+
+        web.loadUrl("http://www.baidu.com");
+    }
+}
